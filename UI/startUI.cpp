2bf9f06ddc1a331d81a16743c5930f469b73a14c
Elaguy <aschultz086@gmail.com>
2019-12-28 21:59:04 -0500

    Beginnings of the Start Screen

--- UI/startUI.cpp
+++ UI/startUI.cpp
@@ -3,18 +3,57 @@
 
 using namespace std;
 
-StartUI::StartUI() {
-    set_title("VWCP-UI");
-    
-    set_border_width(75);
-
-    set_default_size(800, 480);
+StartUI::StartUI()
+    : mainBox(Gtk::ORIENTATION_VERTICAL),
+      bkgd("img/bkgd.jpg")
+{
+    basicSetup();
+    addMainWidgets();
 }
 
 StartUI::~StartUI() {
 
 }
 
-void StartUI::onButtonClicked() {
-    cout << "Hello World [Test]" << endl; 
+void StartUI::basicSetup() {
+    set_title("VWCP-UI");
+    
+    set_size_request(800, 480);
+    
+    set_resizable(false);
+}
+
+void StartUI::addMainWidgets() {
+    mainLayout.set_size(800, 480);
+
+    add(mainLayout);
+    
+    topbar.set_name("topbar");
+    topbar.set_size_request(800, 48);
+    
+    body.set_name("body");
+    body.set_size_request(800, 432);
+    
+    setCSS(topbar, "startUIStyle.css");
+    setCSS(body, "startUIStyle.css");
+
+    mainBox.pack_start(topbar);
+    mainBox.pack_start(body);
+
+    mainLayout.add(mainBox);
+    
+    topbar.show();
+    body.show();
+    mainBox.show();
+    mainLayout.show();
+}
+
+// associates the css file pointed to by 'filename' with the given 'widget'
+void StartUI::setCSS(const Gtk::Widget& widget, const Glib::ustring& filename) {
+    Glib::RefPtr<Gtk::StyleContext> styleContext = widget.get_style_context();
+    Glib::RefPtr<Gtk::CssProvider> cssProvider = Gtk::CssProvider::create();
+
+    cssProvider->load_from_path(filename);
+    styleContext->add_provider(cssProvider, GTK_STYLE_PROVIDER_PRIORITY_USER);
+    styleContext->context_save();
 }
--- UI/startUI.h
+++ UI/startUI.h
@@ -1,9 +1,18 @@
 #ifndef STARTUI_H
 #define STARTUI_H
 
-#include <gtkmm/button.h>
 #include <gtkmm/window.h>
 #include <gtkmm/applicationwindow.h>
+#include <gtkmm/box.h>
+#include <gtkmm/toolbar.h>
+#include <gtkmm/button.h>
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/widget.h>
+#include <glibmm/ustring.h>
+#include <gtkmm/stylecontext.h>
+#include <gtkmm/cssprovider.h>
+#include <gtkmm/image.h>
+#include <gtkmm/layout.h>
 
 class StartUI : public Gtk::ApplicationWindow {
  public:
@@ -11,11 +20,18 @@ class StartUI : public Gtk::ApplicationWindow {
     virtual ~StartUI();
 
  protected:
+    void basicSetup();
+    void addMainWidgets();
+    void setCSS(const Gtk::Widget& widget, const Glib::ustring& filename);
+    
     // signal handlers:
-    void onButtonClicked();
-
+    
     // member widgets:
-    Gtk::Button button;
+    Gtk::Layout mainLayout;
+    Gtk::Box mainBox;
+    Gtk::Image bkgd;
+    Gtk::Toolbar topbar;
+    Gtk::Layout body;
 };
 
 #endif
