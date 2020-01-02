4fc054b558b657e2c8677f993ecbf8bc1b9c6aca
Elaguy <aschultz086@gmail.com>
2020-01-02 11:53:32 -0500

    NON-FUNCTIONAL UI Completed

--- UI/startUI.cpp
+++ UI/startUI.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 StartUI::StartUI()
     : mainBox(Gtk::ORIENTATION_VERTICAL),
-      bkgd("img/bkgd_cropped.jpg")
+      topbar(Gtk::ORIENTATION_HORIZONTAL),
+      vwcpLabel("VWCP", Gtk::Align::ALIGN_START, Gtk::Align::ALIGN_CENTER),
+      dateLabel("Monday, Dec 23 2019", Gtk::Align::ALIGN_END, Gtk::Align::ALIGN_CENTER),
+      bodyContainer(Gtk::ORIENTATION_VERTICAL),
+      body(Gtk::ORIENTATION_VERTICAL),
+      greetingLabel("Good Morning", Gtk::Align::ALIGN_CENTER, Gtk::Align::ALIGN_START),
+      timeLabel("10:00 am", Gtk::Align::ALIGN_CENTER, Gtk::Align::ALIGN_START),
+      driveButton("Drive"),
+      warningBox(Gtk::ORIENTATION_HORIZONTAL),
+      warningSym("img/warningSym.png"),
+      warningLabel("Always drive safe and do not drive distracted.", Gtk::Align::ALIGN_START, Gtk::Align::ALIGN_CENTER),
+      bkgd("img/bkgd_cropped.jpg"),
+      APP_WIDTH(800),
+      APP_HEIGHT(480)
 {
     basicSetup();
     addMainWidgets();
@@ -15,26 +28,95 @@ StartUI::~StartUI() {
 
 }
 
+int StartUI::getAppWidth() {
+    return APP_WIDTH;
+}
+
+int StartUI::getAppHeight() {
+    return APP_HEIGHT;
+}
+
 void StartUI::basicSetup() {
     set_title("VWCP-UI");
     
-    set_size_request(800, 480);
+    set_size_request(APP_WIDTH, APP_HEIGHT);
     
     set_resizable(false);
+
+    //fullscreen();
 }
 
 void StartUI::addMainWidgets() {
     topbar.set_name("topbar");
-    topbar.set_size_request(800, 48);
+    topbar.set_size_request(APP_WIDTH, APP_HEIGHT * 0.10);
+
+    vwcpLabel.set_name("vwcpLabel");
+    vwcpLabel.set_padding(10, 0);
+
+    dateLabel.set_name("dateLabel");
+    dateLabel.set_padding(10, 0);
     
-    bodyBox.set_name("bodyBox");
-    bodyBox.set_size_request(800, 432);
+    topbar.pack_start(vwcpLabel);
+    topbar.pack_start(dateLabel);
+
+    bodyContainer.set_name("bodyContainer");
+    bodyContainer.set_size_request(APP_WIDTH, APP_HEIGHT * 0.90);
+    
+    body.set_name("body");
+    //body.set_size_request(APP_WIDTH, APP_HEIGHT * 0.90);
+    body.set_halign(Gtk::Align::ALIGN_CENTER);
+    body.set_valign(Gtk::Align::ALIGN_CENTER);
+    body.set_spacing(APP_HEIGHT * 0.10);
+    
+    bodyContainer.pack_start(body);
+    
+    greetingLabel.set_name("greetingLabel");
+    //greetingLabel.set_padding(0, 46);
+
+    timeLabel.set_name("timeLabel");
+    //timeLabel.set_padding();
+
+    driveButton.set_name("driveButton");
+    driveButton.set_halign(Gtk::Align::ALIGN_CENTER);
+    driveButton.set_valign(Gtk::Align::ALIGN_START);
+    driveButton.get_child()->set_name("driveButtonLabel");
+    driveButton.set_size_request(208, 42);
+    //driveButton.set_padding();
+
+    warningBox.set_name("warningBox");
+    warningBox.set_halign(Gtk::Align::ALIGN_CENTER);
+    warningBox.set_valign(Gtk::Align::ALIGN_START);
+    warningBox.set_spacing(15);
+    
+    warningSym.set_alignment(Gtk::Align::ALIGN_START, Gtk::Align::ALIGN_CENTER);
+    //warningSym.set_padding();
+    
+    warningLabel.set_name("warningLabel");
+    //warningLabel.set_padding();
+
+    warningBox.pack_start(warningSym);
+    warningBox.pack_start(warningLabel);
+    
+    body.pack_start(greetingLabel);
+    body.pack_start(timeLabel);
+    body.pack_start(driveButton);
+    body.pack_start(warningBox);
     
     setCSS(topbar, "startUIStyle.css");
-    setCSS(bodyBox, "startUIStyle.css");
+    setCSS(vwcpLabel, "startUIStyle.css");
+    setCSS(dateLabel, "startUIStyle.css");
 
+    setCSS(bodyContainer, "startUIStyle.css");
+    setCSS(body, "startUIStyle.css");
+    setCSS(greetingLabel, "startUIStyle.css");
+    setCSS(timeLabel, "startUIStyle.css");
+    setCSS(driveButton, "startUIStyle.css");
+    setCSS(*(driveButton.get_child()), "startUIStyle.css");
+    setCSS(warningBox, "startUIStyle.css");
+    setCSS(warningLabel, "startUIStyle.css");
+    
     mainBox.pack_start(topbar);
-    mainBox.pack_start(bodyBox);
+    mainBox.pack_start(bodyContainer);
 
     mainOverlay.add(bkgd);
     mainOverlay.add_overlay(mainBox);
@@ -42,7 +124,16 @@ void StartUI::addMainWidgets() {
 
     bkgd.show();
     topbar.show();
-    bodyBox.show();
+    vwcpLabel.show();
+    dateLabel.show();
+    bodyContainer.show();
+    body.show();
+    greetingLabel.show();
+    timeLabel.show();
+    driveButton.show();
+    warningBox.show();
+    warningSym.show();
+    warningLabel.show();
     mainBox.show();
     mainOverlay.show();
 }
--- UI/startUI.h
+++ UI/startUI.h
@@ -14,25 +14,48 @@
 #include <gtkmm/image.h>
 #include <gtkmm/overlay.h>
 #include <gtkmm/scrolledwindow.h>
+#include <gtkmm/label.h>
+#include <gtkmm/button.h>
 
 class StartUI : public Gtk::ApplicationWindow {
  public:
     StartUI();
     virtual ~StartUI();
-
+    
+    int getAppWidth();
+    int getAppHeight();
+    
  protected:
     void basicSetup();
     void addMainWidgets();
     void setCSS(const Gtk::Widget& widget, const Glib::ustring& filename);
+    
+    // overrides:
     
     // signal handlers:
     
     // member widgets:
     Gtk::Overlay mainOverlay;
     Gtk::Box mainBox;
-    Gtk::Toolbar topbar;
-    Gtk::Box bodyBox;
+
+    Gtk::Box topbar;
+    Gtk::Label vwcpLabel;
+    Gtk::Label dateLabel;
+
+    Gtk::Box bodyContainer;
+    Gtk::Box body;
+    Gtk::Label greetingLabel;
+    Gtk::Label timeLabel;
+    Gtk::Button driveButton;
+    Gtk::Box warningBox;
+    Gtk::Image warningSym;
+    Gtk::Label warningLabel;
+    
     Gtk::Image bkgd;
+    
+ private:
+    const int APP_WIDTH;
+    const int APP_HEIGHT;
 };
 
 #endif
